6e717ea3c3084d0443074e4959549e3f55f31472
regionkr <regionkr@630f1074-03e4-11e3-b213-59acc461e2ae>
2016-07-23 06:59:29 +0000

    Часть товаров на главной скрываем под спойлер - для seo

--- bitrix/templates/.default/components/bitrix/catalog.section.list/nost/template.php
+++ bitrix/templates/.default/components/bitrix/catalog.section.list/nost/template.php
@@ -1,6 +1,11 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 			<div class="row proizv">
+<? $curPage = $APPLICATION->GetCurPage(true);
+if ($curPage == '/index.php') : ?>
 				<h1 class="h2" style="font-size:28px; color:#000;">Шашки на такси</h1>
+<? else: ?>
+				<h1 class="h2" style="font-size:28px; color:#000;">Наша продукция</h1>
+<?endif;?>
 					<div class="our-products">
 <script type="text/javascript">
 	$j23 = jQuery.noConflict();
@@ -32,6 +37,21 @@ for($i=0; $i<5; $i++) {
 		$arFilter = Array("IBLOCK_ID"=>2, "SECTION_ID"=>$arProducts[$arSection["ID"]][$i][0], "ID"=> $arProducts[$arSection['ID']][$i][1], "ACTIVE_DATE"=>"Y", "ACTIVE"=>"Y");
 	$res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>5), $arSelect);
 	$res_res[$i] = $res;
+}
+// adl 23.07.2016 - получаем всю категорию шашки на такси и шашки на такси премиум, чтобы спрятать её под спойлер на главной (для продвижения)
+if ($curPage == '/index.php') {
+	$arSelect = Array("ID", "IBLOCK_ID", "NAME", "DATE_ACTIVE_FROM", "CODE", "DETAIL_PICTURE", "PROPERTY_*");
+
+	// Обычные шашки для такси
+	if ($arSection['ID'] == 6) {
+		$arFilter = Array("IBLOCK_ID"=>2, "SECTION_ID"=>$arSection["ID"], "INCLUDE_SUBSECTIONS" => "Y", "ACTIVE_DATE"=>"Y", "ACTIVE"=>"Y");
+		$resShashki = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>5000), $arSelect);
+	}
+	// Шашки для такси премиум
+	if ($arSection['ID'] == 39) {
+		$arFilter = Array("IBLOCK_ID"=>2, "SECTION_ID"=>$arSection["ID"], "INCLUDE_SUBSECTIONS" => "Y", "ACTIVE_DATE"=>"Y", "ACTIVE"=>"Y");
+		$resShashkiPremium = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>5000), $arSelect);
+	}
 }
 ?>
 
@@ -98,6 +118,64 @@ if (($arSection["ID"] == 6)||($arSection["ID"] == 39)) {
 						</div>
 <?}?>
 						<div class="clearfix"></div>						
+<? // Часть товаров скрываем под спойлер - это нужно для продвижения сайта 
+  if (($curPage == '/index.php')&&((($arSection["ID"] == 6)||($arSection["ID"] == 39)))) { ?>
+						<a href="javascript:void();" onclick="document.getElementById('addShaski<?=$arSection["ID"];?>').style.display='block'; this.style.display='none';" class="shashki-show">Показать все шашки для такси</a>
+                                                <div id="addShaski<?=$arSection["ID"];?>" style="display:none;">
+<?
+// Берем массив шашек на такси, либо шашек на такси премиум
+if ($arSection["ID"] == 6)
+	$tmpRes = $resShashki;
+else
+	$tmpRes = $resShashkiPremium;
+
+while($ob1 = $tmpRes->GetNextElement()) {
+ $arFields1 = $ob1->GetFields();  
+ $arProps1 = $ob1->GetProperties(); 
+ 
+ // Вычисляем цену "от" - самая минимальная цена, всегда 3-ая позиция с строчке скидок берется
+ $price_from1 = $arProps1["PRICE"]["VALUE"];
+ if (trim($arProps1["DISCOUNT_TABLE"]["VALUE"]) <> '') {
+	$arDiscount1 = preg_split("/[:;]/" , $arProps1["DISCOUNT_TABLE"]["VALUE"]); 
+	// adl 05.04.15 В $arDiscount1[5] могуть быть проценты и не проценты, также там может быть два значения
+	$arDisc1 = explode("," , $arDiscount1[5]);
+	$discount1 = ($arDisc1[1] == "") ? $arDisc1[0] : $arDisc1[1];
+
+	// Разбираем в процентах у нас скидки или в абсолютных значениях
+	if (strpos($discount1, "%") === false)
+		$price_from1 = $discount1;
+	else
+		$price_from1 = $arProps1["PRICE"]["VALUE"] - $arProps1["PRICE"]["VALUE"]*$discount1/100;
+ }
+
+
+
+$res1 = CIBlockElement::GetList(array(), array('ID'=>$arFields1['ID']), false, false, array('ID', 'IBLOCK_ID', 'NAME', 'DETAIL_PAGE_URL'));
+if ($arElement = $res1->GetNext())
+{
+   $full_path1 = $arElement['DETAIL_PAGE_URL'];
+}
+
+$arFields1["DETAIL_PICTURE"] = CFile::GetFileArray($arFields1["DETAIL_PICTURE"]);
+
+// adl 27.09.15 Добавляем надпись "Шашки на такси"
+$add_text1 = "Шашки на такси<br/>";
+?>
+						<div class="col-xs-6">
+							<a class="item anchor" href="<?=$full_path1?>">
+								<img src="/thumb/160x104xcut<?=$arFields1["DETAIL_PICTURE"]["SRC"]?>" alt="<?=$arFields1["NAME"];?>" title="<?=$arFields1["NAME"];?>" class="product-img" />
+								<span class="product-name"><?=$add_text1;?>&#8220;<?=$arFields1["NAME"];?>&#8221;</span>
+								<p class="product-price">от <?=$price_from1;?> р</p>
+								<span class="product-size"><?=$arProps1["SIZE"]["VALUE"];?></span>
+							</a>
+						</div>
+
+<? } // end while ?>
+						</div>
+						<div class="clearfix"></div>						
+
+<? } ?>
+
 					</div>
 				</div>
 			</div>
