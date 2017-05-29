ef06333951955a7efde52f2e6654267802b49156
regionkr <regionkr@630f1074-03e4-11e3-b213-59acc461e2ae>
2017-05-29 12:22:31 +0000

    Меняем номера телефонов на 8800 и 650764

--- bitrix/templates/romack_template/header.php
+++ bitrix/templates/romack_template/header.php
@@ -58,7 +58,8 @@ $curPage = $APPLICATION->GetCurPage(true);
 					</div>
 					<div class="col-xs-10" id="cart">
 						<address>
-						<span id="tel">8 (909) <span style="color:rgb(227,42,42);">714-98-40</span></span><br/>
+						<span id="tel">8 (800) <span style="color:rgb(227,42,42);">200-32-21</span></span><br/>
+						<span style="display: block; margin-top: -10px; margin-bottom: -25px;">Звонок по России бесплатный</span>
 						</address>
 					</div>
 					<div class="col-xs-7 text-left" style="width: 23.33333333%;">
--- romack/sect_contacts_top.php
+++ romack/sect_contacts_top.php
@@ -1,5 +1,5 @@
 					<address>
 						<span class="email">orders@romack.ru</span>
-						<span class="phone">+7 (909) 714-98-40</span>
+						<span class="phone">+7 (3412) 65-07-64</span>
 						<p class="workhours">Пн-Пт с 9:00 до 18:00<br>(по Москве)</p>
 					</address>
