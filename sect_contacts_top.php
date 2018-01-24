aae3873d087e0e40070fe8799d925a66a2938f2e
regionkr_electron2 <regionkr_electron2@630f1074-03e4-11e3-b213-59acc461e2ae>
2018-01-24 04:50:49 +0000

    Доработки

--- sect_contacts_top.php
+++ sect_contacts_top.php
@@ -1,6 +1,6 @@
 					<address>
 						<span class="email">558963@nost.ru</span>
 						<span class="phone">8 (499) 490-64-96</span>
-						<p class="workhours">Пн-Пт с 9:00 до 18:00<br>(по Москве)</p>
+						<p class="workhours">Пн-Пт с 8:00 до 17:00<br>(по Москве)</p>
 						<span class="skype">nost.ru</span>
 					</address>
